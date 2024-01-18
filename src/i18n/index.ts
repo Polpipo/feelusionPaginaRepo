88cf845c2268024a49e014b035c6f1e6bc174a7f
Pol Pipo <polpipo4@gmail.com>
2024-01-18 13:23:06 +0100

    primera instalación de idiomas

---
+++ src/i18n/index.ts
@@ -0,0 +1,23 @@
+import spanish from './es.json'
+import catalan from './ca.json'
+import english from './en.json'
+
+
+
+const LANGUAGES = {
+  CATALAN: 'ca',
+  SPANISH: 'es',
+  ENGLISH: 'en'
+}
+
+export const getI18N = ({
+  currentLocale = 'es'
+}: {
+  currentLocale: string | undefined
+}) => {
+  if (currentLocale === LANGUAGES.SPANISH) return spanish
+  if (currentLocale === LANGUAGES.CATALAN) return catalan
+  if (currentLocale === LANGUAGES.ENGLISH) return english
+  
+  return spanish
+}
